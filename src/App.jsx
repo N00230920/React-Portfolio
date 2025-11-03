7062488423fe776b53818c1178158d138d9e8bf0
Christine Montcheu (Student) <N00230920@iadt.ie>
2025-11-03 17:00:43 +0000

    portfolio start

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import PageNotFound from "./pages/PageNotFound";
+import ProjectIndex from "@/pages/projects/index";
 
 //Import components
 import NavBar from './components/NavBar';
@@ -21,6 +22,12 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/projects" element={<ProjectIndex />} />
+
+        {/* projects */}
+
+
+
       </Routes>
       <Footer />
     </Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,29 @@
 import  {Link} from 'react-router-dom'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 
 const NavBar = () => {
     return (
         <div>
+            <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+            </NavigationMenu>
             <Link to = "/">Home</Link>
             <Link to = "/about">About</Link>
             <Link to = "/projects">Projects</Link>
